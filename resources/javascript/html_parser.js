7385ff7462d9499b33ab5484c9d1964182ef80b7
Jason C <j_cowan06@hotmail.com>
2013-04-02 11:59:22 -0600

    logs of changes, woot

--- resources/javascript/html_parser.js
+++ resources/javascript/html_parser.js
@@ -1,3 +1,7 @@
+/**
+* Main object in control of everything
+* Every other object used in the project is namespaced from this
+*/
 var html_parser = {
 
 	parse_file_location:'',
@@ -5,10 +9,18 @@ var html_parser = {
 	
 	init:function(params){
 		this.parse_file_location = params.parse_file_location;
-		this.load_parse_file();
+		
+		this._load_parse_file();
+		
+		this._load_templates();
+		
 	},
 	
-	load_parse_file:function(){
+	_build_initial:function(){
+		html_parser.control_panel.init();
+	},
+	
+	_load_parse_file:function(){
 		var self = this;
 		$.ajax({
 		  url: this.parse_file_location,
@@ -19,103 +31,107 @@ var html_parser = {
 			alert("Could not read in text file: "+text_status);
 			console.log(text_status);
 		});
-	}
-}
-
-// {url:['url1','url2']}
-
-var html_parser_search = {
-
-	current_check:null,
-	results:[],
-
-	search:function(search_obj){
-		this.results = [];
-		for(var i in html_parser.parsed_data.urls){
-			this._search(html_parser.parsed_data.urls[i], search_obj, 0);
-		}
-		return this.results;
 	},
 	
-	_search:function(dataset, search_obj, current_depth){
-		
-		if(current_depth == 0){
-			this.current_check = dataset
-		}
-		
-		for(var i in dataset){
-			//console.log(current_depth);
-			console.log("index: "+i);
-			/*console.log(dataset[i]);
-			console.log(search_obj[i]);*/
-			if(typeof dataset[i] !== 'undefined'){
-				
-				if(Object.prototype.toString.call(dataset[i]) == "[object Object]"){
-					
-					var result = this._search(dataset[i], search_obj[i], current_depth+1);
-					if(result === true){
-						break;
-					}
-				}
-				else if(Object.prototype.toString.call(dataset[i]) === '[object Array]'){
-				
-				}
-				/*else{
-					console.log("dataset value: "+dataset[i]);
-				}*/
-				else if(typeof search_obj[i] !== 'undefined'){
-					console.log('poop');
-					var current = search_obj[i];
-					if(typeof search_obj[i] === 'object'){
-						for(var i in current){
-							if(typeof current === 'object'){
-								current = current[i];
-							}
-							else if(current == dataset[i]){
-								this.results.push(this.current_check);
-								return true;
-							}
-						}
-					}
-					else if(current == dataset[i]){
-						this.results.push(this.current_check);
-						return true;
-					}
-				}
-			}
-		}
-		
-		return false;
-	}
+	_load_templates:function(){
+		html_parser.templates.get_templates(this._build_initial);
+	},
 }
 
-html_parser_search_obj = function(){
+html_parser:loader{
+	have this do the loading and the gif for loading
+}
+
+
+/**
+*	Object in charge of managing the control panel
+*/
+html_parser.control_panel = {
+	init:function(){
+		
+	},
 	
-	this.where = function(key, value){
+	build:(){
+		have this build out the control panel
+	}
+	
+	
+}
+
+/**
+* Object in charge of managing templates
+*/
+html_parser.templates = {
+
+	loaded_count:0,
+	
+	/**
+	* Templates to be loaded
+	* To add another, simply add another variable to the temps object.  The variable name should be the name of the template file in resources/templates
+	*/
+	temps:{
+		control_panel_skeleton:'',
+		control_panel_general_stats:''
+	},
+	
+	/**
+	* Gets a template based off the templates name
+	*/
+	get_template:function(which){
+		return this.temps[which];
+	},
+	
+	get_templates:function(callback){
+		for(var template_name in this.temps){
+			this._load_template(template_name, callback);
+		}
+	},
+	
+	_load_template:function(template_name, callback){
+		var self = this;
+		$.get('resources/templates/'+template_name, function(data) {
+				self._template_loaded(template_name, data, callback);
+		});
+	},
+	
+	_template_loaded:function(template_name, data, callback){
+		this.temps[template_name] = data;
+		this.loaded_count++;
 		
-		var parts = key.split('.');
-		var current = null;
-		var counter = 1;
+		var size = 0;
+		for(var i in this.temps){size++;}
 		
-		for(var i in parts){
-			if(current === null && typeof this[parts[i]] == 'undefined'){
-				this[parts[i]] = {};
-			}
-			else if(current !== null && typeof current[parts[i]] == 'undefined'){
-				current[parts[i]] = {};
-			}
-			
-			if(counter == parts.length){
-				current = (current === null)?(this[parts[i]] = value):(current[parts[i]] = value);
-			}
-			else{
-				current = (current === null)?this[parts[i]]:current[parts[i]];
-			}
-			counter++;
+		if(size == this.loaded_count){
+			callback();
 		}
 	}
 }
+ 
 
-cat = new html_parser_search_obj();
-cat.where('html_asset.title', 'One Page');
-cat.where('content_type', 'image/png');
+/**
+* Object in charge of searching data
+*/
+html_parser.search = {
+	
+	/**
+	*	Returns all the urls from the parsed data
+	*/
+	get_urls:function(){
+		return html_parser.parsed_data.urls;
+	},
+	
+	/**
+	*	Returns any url that contained a misspelling
+	*/
+	get_all_misspellings:function(){
+		var result = [];
+		var urls = this.get_urls()
+		for(var i in urls){
+			console.log(urls[i]);
+			if(urls[i].html_asset != null && urls[i].html_asset.misspellings.length > 0){
+				result.push(urls[i]);
+			}
+		}
+		return result;
+	}
+}
